22fe59a6f67926bf4d8b558500101e519b2b39af
joacojachow <joacojachow@hotmail.com>
2025-08-16 02:06:04 -0300

    navbar name fixed

--- src/pages/productos/index.js
+++ src/pages/productos/index.js
@@ -1,5 +1,5 @@
 import Productos from 'src/Components/Productos/Productos'
-import Nabvar from 'src/Components/Navbar/Navbar'
+import Navbar from 'src/Components/Navbar/Navbar'
 import Footer from 'src/Components/Footer/Footer'
 import Head from 'next/head'
 
@@ -11,7 +11,7 @@ const index = () => {
       </Head>
       <div className='relative bg-fixed bg-center bg-cover bg-ProductosFondo'>
         <div className='relative bg-black bg-opacity-30'>
-          <Nabvar />
+          <Navbar />
           <h1 className='pb-6 text-5xl font-bold text-center text-white whitespace-nowrap md:pb-8 md:text-7xl lg:text-start lg:pt-8 xl:pt-16 xl:pb-16 lg:ml-20 xl:ml-36 2xl:ml-52'>Productos</h1>
         </div>
       </div>
