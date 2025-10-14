f8d4c0701d30e354c37f7f73d9db1418403ab05c
joacojachow <joacojachow@hotmail.com>
2025-10-14 12:53:40 -0300

    card improved

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,25 +1,34 @@
 import React, { useState } from 'react'
 import Link from 'next/link'
 import { motion } from 'framer-motion'
+import OptimizedImage from '../OptimizedImage/OptimizedImage'
 
 const Card = ({ name, img, link }) => {
   const [isHovered, setIsHovered] = useState(false)
+
   return name && img && link
     ? (
       <div data-aos='flip-left' className='shadow-xl card w-96 bg-[#DA9F6D] max-h-96 rounded-2xl'>
         <Link href={`/productos/${link}`}>
           <div>
-            <figure>
-              <motion.img
-                className='w-full h-full bg-center bg-cover max-h-64 rounded-t-2xl'
-                src={img?.src}
-                alt={img}
-                loading='lazy'
+            <figure className='relative h-64 overflow-hidden rounded-t-2xl'>
+              <motion.div
+                className='w-full h-full'
                 onMouseEnter={() => setIsHovered(true)}
                 onMouseLeave={() => setIsHovered(false)}
                 animate={{ scale: isHovered ? 1.1 : 1 }}
                 transition={{ duration: 0.3 }}
-              />
+              >
+                <OptimizedImage
+                  src={img}
+                  alt={`${name} - Tajamar Molduras`}
+                  className='w-full h-full rounded-t-2xl'
+                  width={384}
+                  height={256}
+                  quality={80}
+                  sizes='(max-width: 768px) 100vw, 384px'
+                />
+              </motion.div>
             </figure>
           </div>
         </Link>
