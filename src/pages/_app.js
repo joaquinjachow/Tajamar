d0869344e1102ebfacdddb704d687ef7a5dd6866
joacojachow <joacojachow@hotmail.com>
2025-10-16 13:55:38 -0300

    wthatsApp button in app added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,11 @@
 import '@styles/globals.css'
+import WhatsAppButton from 'src/Components/Icons/Whatsaap'
+
 export default function App ({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <WhatsAppButton />
+    </>
+  )
 }
